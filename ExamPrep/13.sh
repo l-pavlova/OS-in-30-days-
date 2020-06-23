34af64e15fa59b0fb8164ea8ba630c0f7ec1ae33
Me <me@examle.com>
2020-06-23 03:30:21 +0300

    Add symlink with nonexistent destination task solution

--- ExamPrep/13.sh
+++ ExamPrep/13.sh
@@ -8,3 +8,13 @@ if [[ -f $1 ]]; then
 	echo not a dir
 	exit 2
 fi
+#find all symlinks
+#find the nonexistent dest wit if -e 
+links=$(ls -lR $dir| egrep ">")
+for curr in $links; do
+	dest=$(echo $curr | cut -d ">" -f 2 )
+	if [ -e $dest ]; then
+		echo $curr
+	fi
+done
+
